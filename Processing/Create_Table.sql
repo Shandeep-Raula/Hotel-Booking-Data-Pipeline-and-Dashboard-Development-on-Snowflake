d542079463101f01d4f8e8671e908fba4c795dfd
Shandeep-Raula <shandeepra@gmail.com>
2025-12-22 18:10:50 +0530

    added raw_file into snowflake stage and load satge to table using copy into command

---
+++ Processing/Create_Table.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE HOTEL.PUBLIC.BRONZE_HOTEL_BOOKING (
+    booking_id STRING,
+    hotel_id STRING,
+    hotel_city STRING,
+    customer_id STRING,
+    customer_name STRING,
+    customer_email STRING,
+    check_in_date STRING,
+    check_out_date STRING,
+    room_type STRING,
+    num_guests STRING,
+    total_amount STRING,
+    currency STRING,
+    booking_status STRING
+);
